f66d90f5cf8270990335836ecd6cfff9d79dd97c
CodeSlayer123 <3613@holbertonschool.com>
2021-09-27 10:50:40 -0700

    Capitalized

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,14 +2,11 @@
 /**
  * main - Prints "Programming is like building a multilingual puzzle"
  *
- * return 0
+ * Return: 0
  */
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-/**
- *return 0 if positive
- /*
 	return (0);
 }
