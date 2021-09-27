c86794b8e403ffb8727cb451b87b0cd2c8a7f761
CodeSlayer123 <3613@holbertonschool.com>
2021-09-27 10:41:44 -0700

    Betty comments

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+/**
+ * main - Prints "Programming is like building a multilingual puzzle" using puts command
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
