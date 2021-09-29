699e2263772bb7e6db8927ee9c32f70d2dab9a30
CodeSlayer123 <3613@holbertonschool.com>
2021-09-29 11:57:25 -0700

    4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isalpha - checks if c is lower or upper
+ *
+ * Return: this is what's supposed to be returned to main
+ */
+
+	int _isalpha(int c)
+{
+
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
