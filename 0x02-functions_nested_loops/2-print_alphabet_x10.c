b70ca82b9ecc6d0939a6e56a4e6fd24209ea9c5d
CodeSlayer123 <3613@holbertonschool.com>
2021-09-29 11:00:28 -0700

    redid for loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,11 +7,16 @@
 
 void print_alphabet_x10(void)
 {
+	char ch;
 	int n;
 
-	for (n = 0; n <= 10; n++)
-	{
-	  print_alphabet();
+	for (n = 0; n < 10; n++)
+        {
+		for (ch = 'a'; ch <= 'z'; ch++)  
+		{
+		_putchar(ch);
+		}
+	_putchar('\n');
 	}
 
 }
