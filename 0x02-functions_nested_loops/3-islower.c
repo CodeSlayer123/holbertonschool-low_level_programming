0feacda10e001e860acb26ccb58a4d72359a7b08
CodeSlayer123 <3613@holbertonschool.com>
2021-09-29 11:46:02 -0700

    3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _islower - checks if c is lower or upper
+ *
+ * Return: this is what's supposed to be returned to main
+ */
+
+	int _islower(int c)
+{
+
+	if (c >= 'A' && c <= 'Z')
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
