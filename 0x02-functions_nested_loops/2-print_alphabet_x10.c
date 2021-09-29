c316ab2fe7f9e29a42009581f4d2d82cb84206e2
CodeSlayer123 <3613@holbertonschool.com>
2021-09-29 10:35:08 -0700

    second commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints alphabet 10 times
+ *
+ * Return: this is what's supposed to be returned to main
+ */
+
+void print_alphabet_x10(void)
+{
+	char n;
+
+	for (n = '0'; n <= "10"; ch++)
+	{
+	print_alphabet(void)
+	}
+
+}
