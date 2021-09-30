daf3dd4812d754c5407b5678d4c7fc3a907672d0
CodeSlayer123 <3613@holbertonschool.com>
2021-09-30 09:06:14 -0700

    8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,18 +1,44 @@
 #include "main.h"
+#include <stdio.h>
 /**
- * add - adds two numbers together
- *
- * @x: number
- *
- * @y: number
+ * jack_bauer - prints clock in 24 hour time
  *
  * Return: this is what's supposed to be returned to main
  */
 
-int add(int x, int y)
+void jack_bauer(void)
 {
 
-	int sum = x + y;
+	int hour;
 
-	return (sum);
+
+	for (hour = 0; hour < 23; hour++)
+	{
+	int minute = 0;
+
+	for (minute = 0; minute < 59; minute++)
+		{
+		if (hour < 10)
+		{
+		printf("0%i:", hour);
+		}
+
+		else
+		{
+
+	printf("%i:", hour);
+	}
+
+	if (minute < 10)
+	{
+	printf("0%i\n", minute);
+	}
+	else
+	{
+	printf("%i\n", minute);
+	}
+	}
+	}
+
+
 	}
