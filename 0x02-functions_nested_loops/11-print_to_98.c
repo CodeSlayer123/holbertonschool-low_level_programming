1a74cf24c2578a4e9301fb1a8b9192b9a72a4797
CodeSlayer123 <3613@holbertonschool.com>
2021-09-30 17:36:27 -0700

    11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,18 +1,52 @@
 #include "main.h"
+#include <stdio.h>
 /**
- * add - adds two numbers together
+ * print_to_98 - prints numbers to 98
  *
- * @x: number
- *
- * @y: number
+ * @n: number
  *
  * Return: this is what's supposed to be returned to main
  */
 
-int add(int x, int y)
+void print_to_98(int n)
 {
-
-	int sum = x + y;
-
-	return (sum);
+	if (n < 98)
+	{
+		while (n <= 98)
+		{
+			if (n == 98)
+			{
+			printf("%i", n);
+			n++;
+			}
+			else
+			{
+			printf("%i, ", n);
+			n++;
+			}
+		}
+		putchar('\n');
 	}
+	else if (n == 98)
+	{
+	printf("%i\n", n);
+	}
+
+	else if (n > 98)
+	{
+		while (n >= 98)
+		{
+			if (n == 98)
+			{
+			printf("%i", n);
+			n--;
+			}
+			else
+			{
+			printf("%i, ", n);
+			n--;
+			}
+		}
+		putchar('\n');
+	}
+}
