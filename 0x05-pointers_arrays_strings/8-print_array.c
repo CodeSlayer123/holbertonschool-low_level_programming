036cdd5fa3d8c0e6cf7db7f85436e8bd8b4045a4
CodeSlayer123 <3613@holbertonschool.com>
2021-10-07 07:54:06 -0700

    8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,10 +12,18 @@
 
 void print_array(int *a, int n)
 {
-	while (*a <= n)
+  int i;
+
+  for (i = 0; i < n; i++)
 	{
-	  printf("%d", *a);
-		a++;
+	  if (i != n - 1)
+	    {
+	  printf("%d, ", a[i]);
+	    }
+	  else
+	    {
+	      printf("%d", a[i]);
+	    }
 	}
 	putchar('\n');
 }
