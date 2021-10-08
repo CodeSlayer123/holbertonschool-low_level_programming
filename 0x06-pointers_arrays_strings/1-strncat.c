76258017eece2b78200af8c0b5c82009b89d65f2
CodeSlayer123 <3613@holbertonschool.com>
2021-10-08 12:13:32 -0700

    0x06

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include "holberton.h"
+/**
+ * _strncat - concatenates two strings, limits src to n bytes
+ *
+ * @dest: first number
+ *
+ * @src: second number
+ *
+ *@n: number of bytes
+ *
+ * Return: Always 0
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (src[j] != '\0' && j < n)
+	{
+	 
+	 
+	    dest[i] = src[j];
+		i++;
+		j++;
+	    }
+	    
+	
+	
+	return (dest);
+}
