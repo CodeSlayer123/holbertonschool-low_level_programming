4e2e7f9c75442bc3e03bff37eb2d5259eddb870c
CodeSlayer123 <3613@holbertonschool.com>
2021-10-11 16:10:35 -0700

    actually 6

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -4,23 +4,20 @@
  *
  * @a: number of rows
  *
- * @8: number of columns
- *
  * Return: Always 0
  */
 void print_chessboard(char (*a)[8])
 {
-  int row;
-  int column;
-  
-  for (row = 0; row < 8; row++)
-    {
-      for (column = 0; column < 8; column++)
+	int row;
+	int column;
+
+	for (row = 0; row < 8; row++)
 	{
-	  _putchar(a[row][column]);
-	  
+		for (column = 0; column < 8; column++)
+		{
+			_putchar(a[row][column]);
+		}
+	_putchar('\n');
 	}
-      _putchar('\n');
-    }	
 
 }
