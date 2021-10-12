72e7f366aa006da2eb3853272d60aee3ab54351e
CodeSlayer123 <3613@holbertonschool.com>
2021-10-11 18:26:27 -0700

    8

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_diagsums - prints sum of two diagonals of square
+ *
+ * @a: number of rows
+ *
+ * @size: size
+ *
+ * Return: Always 0
+ */
+void print_diagsums(int *a, int size)
+{
+_putchar(a);
+_putchar(size);
+
+}
