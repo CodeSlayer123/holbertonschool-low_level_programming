2d0f98eea190e85ec43d8d2e85fd9bb38d6124ea
CodeSlayer123 <3613@holbertonschool.com>
2021-10-11 17:14:49 -0700

    3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -11,21 +11,31 @@
 unsigned int _strspn(char *s, char *accept)
 {
 
-  int i;
-  int j;
-  int length = 0;
+	int i;
+	int j;
+	int length = 0;
 
-for (i = 0; s[i] != '\0'; i++)
-    {
-     for (j = 0; accept[j]; j++)
-       {
-        if (s[i] == s[j])
-          {
-	    length++;
-          }
-       }
-    }
+for (i = 0; s[i] != '\0' ||
+		(s[i  - 1] == ' ' || s[i  - 1] == '\t' || s[i  - 1] == '\n' ||
+		s[i  - 1] == ',' || s[i  - 1] == ';' || s[i  - 1] == '.' ||
+		s[i  - 1] == '!' || s[i  - 1] == '?' || s[i  - 1] == '"' ||
+		s[i  - 1] == '(' || s[i  - 1] == ')' || s[i  - 1] == '{' ||
+		s[i  - 1] == '}')
+; i++)
+{
+
+
+	for (j = 0; accept[j] != '\0'; j++)
+	{
+		if (s[i] == s[j])
+		{
+		length++;
+		}
+	}
+}
 
 	return (length);
 
 }
+
+
