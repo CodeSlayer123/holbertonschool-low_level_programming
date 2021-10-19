c3f5b531e097183f153d9a518fece794bb1dc650
CodeSlayer123 <3613@holbertonschool.com>
2021-10-19 12:32:40 -0700

    100 betty

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,31 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+
+int makecoins(int money);
+
 /**
  * main - prints the program's name
  * @argc: argument count
  * @argv: array of arguments passed from terminal
  * Return: On success
  */
-int makecoins(int money);
+
 
 int main(int argc, char *argv[])
 {
 	int coins;
 	int money;
 
-if (argc != 2)
-      {
-          printf("Error\n");
-          return (1);
-      }
-money = atoi(argv[1]);
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	money = atoi(argv[1]);
 
 	if (money < 0)
 	{
 		printf("0\n");
 		return (0);
 	}
+
 	coins = makecoins(money);
 	printf("%d\n", coins);
 	return (0);
@@ -33,11 +38,12 @@ money = atoi(argv[1]);
 /**
  * makecoins - make coins from an integer
  * @money: the money we get
- * Rreturns: coins from the money
+ * Return: coins from the money
  */
 int makecoins(int money)
- {
-	 int coins = 0;
+{
+	int coins = 0;
+
 	while (money >= 25)
 	{
 		coins++;
@@ -47,22 +53,22 @@ int makecoins(int money)
 	while (money >= 10)
 	{
 		coins++;
-	money -= 10;
+		money -= 10;
 	}
 	while (money >= 5)
-      {
-          coins++;
-      money -= 5;
-      }
+	{
+		coins++;
+		money -= 5;
+	}
 	while (money >= 2)
-        {
-            coins++;
-        money -= 2;
-        }
+	{
+		coins++;
+		money -= 2;
+	}
 	while (money >= 1)
-        {
-            coins++;
-        money -= 1;
-        }
+	{
+		coins++;
+		money -= 1;
+	}
 	return (coins);
- }
+}
