15551a0d763ab1f2973099f8c9a61a63ee785ef1
CodeSlayer123 <3613@holbertonschool.com>
2021-10-20 12:57:41 -0700

    4

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * free_grid - frees a 2 dimensional array of ints
+ * @grid: width
+ * @height: height
+ * Return: pointer to array, NULL if it fails
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}
