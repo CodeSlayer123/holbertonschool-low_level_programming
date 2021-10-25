cb304bd246d4bc0a2d2b503fdd56e2cd13622a6d
CodeSlayer123 <3613@holbertonschool.com>
2021-10-25 13:27:59 -0700

    2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,16 +11,26 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	int *a;
+	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 	{
 		return ('\0');
 	}
+	if (size < sizeof(int))
+		size = 4;
+
 	a = malloc(nmemb * size);
 
 	if (a == NULL)
 	{
 		return ('\0');
 	}
+
+	for (i = 0; i < nmemb; i++)
+	{
+		a[i] = 0;
+	}
+
 	return (a);
 }
