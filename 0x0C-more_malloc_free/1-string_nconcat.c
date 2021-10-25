44819148b81701f7fa2f213003fd5cfda83d605b
CodeSlayer123 <3613@holbertonschool.com>
2021-10-25 11:01:05 -0700

    1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -7,18 +7,26 @@
  * @s1: first string
  * @s2: second string
  * @n: number of bytes being conatenated
+ * @a: array
  * Return: pointer to allocated memory
  */
+
+char *loop(char *a, char *s1, char *s2, unsigned int n);
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *a;
-	int i;
-	unsigned int j;
-	int number = n;
 	int size1;
 	int size2;
 
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
 	size1 = strlen(s1);
 	size2 = strlen(s2);
 	a = malloc((size1 + size2 + 1) * sizeof(char));
@@ -26,6 +34,26 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		return ('\0');
 	}
+	loop(a, s1, s2, n);
+	return (a);
+}
+
+
+/**
+ * loop - loops the two strings
+ * @a: array
+ * @s1: first string
+ * @s2: second string
+ * @n: number of characters being concatenated
+ * Return: array a
+*/
+char *loop(char *a, char *s1, char *s2, unsigned int n)
+{
+	int size2 = strlen(s2);
+	int number = n;
+	int i;
+	unsigned int j;
+
 	for (i = 0; s1[i] != '\0' ; i++)
 	{
 		a[i] = s1[i];
