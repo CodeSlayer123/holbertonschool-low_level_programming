4d5c56a6e86a199e9e111d471befef6cb6f0d0b1
CodeSlayer123 <3613@holbertonschool.com>
2021-11-12 12:57:27 -0800

    4a

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include "main.h"
+
+int bin_to_dec(int *b);
+int power(int x, unsigned int y);
+
+/**
+ * clear_bit - gets value of a bit at given index
+ * @n: number
+ * @index: index
+ * Return: 1 if it workes or -1 if an error occured
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	int binaryNum[32];
+	int i = 0;
+	int number = *n;
+
+	if (n == NULL || index == '\0')
+	{
+		return (-1);
+	}
+	while (number > 0)
+	{
+		binaryNum[i] = number % 2;
+		number = number / 2;
+		i++;
+	}
+	binaryNum[index] = 0;
+	bin_to_dec(binaryNum);
+	return (1);
+}
+/**
+ * bin_to_dec - converts binary number to unsigned int
+ * @b: points to string of 0s and 1s
+ * Return: the converted number, or 0 if b is null or not a 1 or 0
+ */
+int bin_to_dec(int *b)
+{
+	int dec = 0;
+	int i;
+	int rem;
+	int n = *b;
+
+	if (b == NULL)
+	{
+		return (0);
+	}
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] != '0' && b[i] != '1')
+		{
+			return (0);
+		}
+	}
+	for (i = 0; n != 0; ++i)
+	{
+		rem = n % 10;
+		n /= 10;
+		dec += rem * power(2, i);
+	}
+
+
+
+	return (dec);
+
+
+}
+/**
+ * power - finds the power of int
+ * @x: int
+ * @y: int
+ * Return: power
+ */
+	int power(int x, unsigned int y)
+{
+	if (y == 0)
+		return (1);
+	else if (y % 2 == 0)
+		return (power(x, y / 2) * power(x, y / 2));
+	else
+		return (x * power(x, y / 2) * power(x, y / 2));
+}
