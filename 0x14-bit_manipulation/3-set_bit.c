3dd3beaa1772667635f7432e9649c8a15a389b92
CodeSlayer123 <3613@holbertonschool.com>
2021-11-13 09:28:34 -0800

    3c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -4,9 +4,6 @@
 #include <math.h>
 #include "main.h"
 
-int bin_to_dec(int *b);
-int power(int x, unsigned int y);
-
 /**
  * set_bit - gets value of a bit at given index
  * @n: number
@@ -16,72 +13,12 @@ int power(int x, unsigned int y);
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	int binaryNum[32];
-	int i = 0;
-	int number = *n;
 
 	if (n == NULL || index == '\0')
 	{
 		return (-1);
 	}
-	while (number > 0)
-	{
-		binaryNum[i] = number % 2;
-		number = number / 2;
-		i++;
-	}
-	binaryNum[index] = 1;
-	bin_to_dec(binaryNum);
+
+	*n = (1 << index) | *n;
 	return (1);
-}
-/**
- * bin_to_dec - converts binary number to unsigned int
- * @b: points to string of 0s and 1s
- * Return: the converted number, or 0 if b is null or not a 1 or 0
- */
-int bin_to_dec(int *b)
-{
-	int dec = 0;
-	int i;
-	int rem;
-	int n = *b;
-
-	if (b == NULL)
-	{
-		return (0);
-	}
-	for (i = 0; b[i] != '\0'; i++)
-	{
-		if (b[i] != '0' && b[i] != '1')
-		{
-			return (0);
-		}
-	}
-	for (i = 0; n != 0; ++i)
-	{
-		rem = n % 10;
-		n /= 10;
-		dec += rem * power(2, i);
-	}
-
-
-
-	return (dec);
-
-
-}
-/**
- * power - finds the power of int
- * @x: int
- * @y: int
- * Return: power
- */
-	int power(int x, unsigned int y)
-{
-	if (y == 0)
-		return (1);
-	else if (y % 2 == 0)
-		return (power(x, y / 2) * power(x, y / 2));
-	else
-		return (x * power(x, y / 2) * power(x, y / 2));
 }
