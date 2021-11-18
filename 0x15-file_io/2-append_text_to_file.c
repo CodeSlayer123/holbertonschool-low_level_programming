3e864ee32fe832180c60d22f33f6d75e819c8589
CodeSlayer123 <3613@holbertonschool.com>
2021-11-18 07:33:00 -0800

    added if statement and changed close statement

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -21,6 +21,11 @@ int append_text_to_file(const char *filename, char *text_content)
 	int length;
 	int fd;
 
+	if (text_content == NULL)
+	{
+		return (1);
+	}
+
 	length = strlen(text_content);
 	if (filename == NULL)
 		return (-1);
@@ -28,17 +33,17 @@ int append_text_to_file(const char *filename, char *text_content)
 	fd = open(filename, O_WRONLY | O_APPEND);
 
 	if (fd == -1)
-		return (-1);
-
-	if (text_content !=  NULL)
 	{
-		wr = write(fd, text_content, length);
+		return (-1);
 	}
+
+	wr = write(fd, text_content, length);
+
 
 	if (wr == -1)
 		return (-1);
 
-	close(wr);
+	close(fd);
 
 
 return (1);
