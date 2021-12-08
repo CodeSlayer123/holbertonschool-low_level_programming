c7b2bcc0c0b5a42926b04977ae247131051a91ef
CodeSlayer123 <3613@holbertonschool.com>
2021-12-08 09:02:20 -0800

    forgot the semi colon lol

--- 0x18-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x18-doubly_linked_lists/8-delete_dnodeint.c
@@ -31,7 +31,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	for (i = 0; i != index; i++)
 	{
 		if (tmp->next == NULL)
-			return (-1)
+			return (-1);
 		tmp = tmp->next;
 		futNode = futNode->next;
 
