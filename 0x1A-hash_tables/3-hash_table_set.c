58aa9dea59bf92283ce3215c6ff53b1700d554fd
CodeSlayer123 <3613@holbertonschool.com>
2021-12-13 08:08:51 -0800

    break to return

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -46,7 +46,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 				free(new->key);
 				free(new->value);
 				free(new);
-				break;
+				return (1);
 			}
 			current = current->next;
 		}
