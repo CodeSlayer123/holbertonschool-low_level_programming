d260c59709fb2d5c45f40f9b55cf1b28be9f1f0a
CodeSlayer123 <3613@holbertonschool.com>
2021-12-13 08:42:34 -0800

    free?

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -19,7 +19,10 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	new = malloc(sizeof(hash_node_t));
 	if (new == NULL)
+	{
+		free(new);
 		return (0);
+	}
 	index = key_index((const unsigned char *)key, size);
 	new->key = strdup(key);
 	new->value = strdup(value);
