ad6833aeb4c69b4bdccaa83e62df3070c4506ca7
CodeSlayer123 <3613@holbertonschool.com>
2021-12-13 08:20:52 -0800

    more minor changes

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -15,22 +15,18 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	int size = ht->size;
 	hash_node_t *new, *current;
 
-	if (key == NULL || ht == NULL)
+	if (key == NULL || ht == NULL || strcmp("", key) == 0)
 		return (0);
 	index = key_index((const unsigned char *)key, size);
-
 	new = malloc(sizeof(hash_node_t));
 	if (new == NULL)
 		return (0);
-
 	new->key = strdup(key);
 	new->value = strdup(value);
 	if (new->key == NULL || new->value == NULL)
 		return (0);
-
 	new->next = NULL;
 	current = ht->array[index];
-
 	if (current == NULL)
 		ht->array[index] = new;
 	else
@@ -44,7 +40,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 				free(new->key);
 				free(new->value);
 				free(new);
-				return (1);
+				break;
 			}
 			current = current->next;
 		}
