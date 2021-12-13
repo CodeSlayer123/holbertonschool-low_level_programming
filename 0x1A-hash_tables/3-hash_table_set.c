314d3e6bdb96d65d9ceb5124a1350f4a836a8059
CodeSlayer123 <3613@holbertonschool.com>
2021-12-13 08:36:38 -0800

    removed 3 null checks

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -15,7 +15,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	int size = ht->size;
 	hash_node_t *new, *current;
 
-	if (key == NULL || ht == NULL || strcmp("", key) == 0)
+	if (key == NULL || strcmp("", key) == 0)
 		return (0);
 	index = key_index((const unsigned char *)key, size);
 	new = malloc(sizeof(hash_node_t));
@@ -23,8 +23,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	new->key = strdup(key);
 	new->value = strdup(value);
-	if (new->key == NULL || new->value == NULL)
-		return (0);
 	new->next = NULL;
 	current = ht->array[index];
 	if (current == NULL)
