c10ca94ae618a8a41b5bcab687def10291605b89
CodeSlayer123 <3613@holbertonschool.com>
2021-12-13 08:31:26 -0800

    current vs current->next

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -31,7 +31,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		ht->array[index] = new;
 	else
 	{
-		while (current->next != NULL)
+		while (current != NULL)
 		{
 			if (strcmp(current->key, key) == 0)
 			{
