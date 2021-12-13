d0be14db8aefeb01fd3cd799e5ace0c7630b70b0
CodeSlayer123 <3613@holbertonschool.com>
2021-12-13 08:05:57 -0800

    strcmp added

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,24 +13,20 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
 	int size = ht->size;
-	hash_node_t *new, *current, *before;
+	hash_node_t *new, *current;
 
-	if (key == NULL)
+	if (key == NULL || ht == NULL)
 		return (0);
 	index = key_index((const unsigned char *)key, size);
 
 	new = malloc(sizeof(hash_node_t));
 	if (new == NULL)
 		return (0);
-	new->key = malloc(sizeof(hash_node_t));
-	if (new == NULL || new->key == NULL)
-		return (0);
-	new->value = malloc(sizeof(hash_node_t));
-	if (new == NULL || new->value == NULL)
-		return (0);
 
-	strcpy(new->key, key);
-	strcpy(new->value, value);
+	new->key = strdup(key);
+	new->value = strdup(value);
+	if (new->key == NULL || new->value == NULL)
+		return (0);
 
 	new->next = NULL;
 	current = ht->array[index];
@@ -39,14 +35,23 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		ht->array[index] = new;
 	else
 	{
-		while (current->next != NULL)
+		while (current != NULL)
 		{
+			if (strcmp(current->key, key))
+			{
+				free(current->value);
+				current->value = strdup(value);
+				if (current->value == NULL)
+					return (0);
+				free(new->key);
+				free(new->value);
+				free(new);
+				break;
+			}
 			current = current->next;
 		}
-		before = current;
-		current = current->next;
-		current = new;
-		before->next = new;
+		new->next = ht->array[index];
+		ht->array[index] = new;
 	}
 return (1);
 }
