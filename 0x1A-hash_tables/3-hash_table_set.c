e77b73ee929f50cee9a871e98f4929becc1ea591
CodeSlayer123 <3613@holbertonschool.com>
2021-12-13 08:41:28 -0800

    rearranged malloc and key_index

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -17,10 +17,10 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (key == NULL || strcmp("", key) == 0 || ht == NULL)
 		return (0);
-	index = key_index((const unsigned char *)key, size);
 	new = malloc(sizeof(hash_node_t));
 	if (new == NULL)
 		return (0);
+	index = key_index((const unsigned char *)key, size);
 	new->key = strdup(key);
 	new->value = strdup(value);
 	new->next = NULL;
