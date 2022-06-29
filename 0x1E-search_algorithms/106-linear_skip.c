709e1ff6403857f8565ee452a1ba96e4659a59c6
Code Slayer <3613@holbertonschool.com>
2022-06-29 11:49:58 -0500

    searching algo linear skip

---
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -0,0 +1,40 @@
+#include "search_algos.h"
+
+skiplist_t *linear_skip(skiplist_t *list, int value){
+    skiplist_t *prev = list;
+    skiplist_t *tmp = list->express;
+
+    while (tmp != NULL){
+        /*
+        if (tmp->n == value){
+            return tmp;
+        }
+        */
+        printf("Value checked at index [%ld] = [%d]\n", tmp->index, tmp->n);
+
+        if (value < tmp->n){
+            printf("Value found between indexes [%ld] and [%ld]\n", prev->index, tmp->index);
+
+            while (prev != tmp->next){
+                printf("Value checked at index [%ld] = [%d]\n", prev->index, prev->n);
+
+                if (prev->n == value){
+                    return prev;
+                }
+                prev = prev->next;
+
+                /*if (prev->next == tmp){
+                    return NULL;
+                }
+                */
+            }
+        }
+        else {
+            tmp = tmp->express;
+            prev = prev->express;
+
+        }
+    }
+    return NULL;
+
+}
